142fa587370a6b11162ddba5ae452dbec7d38c86
malashkin.andrey@mail.ru <malashkin.andrey@mail.ru>
2013-10-15 12:57:56 +0400

    experimental part improved

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -7,20 +7,27 @@
 #include "ui_mainwindow.h"
 #include "modelmananger.h"
 
+const int experimentsNumber = 100;
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     m_ui(new Ui::MainWindow)
 {
     m_ui->setupUi(this);
 
-    for(int i = 10; i < 30; i++)
+    for(int i = 7; i < 30; i++)
     {
-        ModelMananger mananger(20);
-        int result = mananger.calcTime();
 
-//        int result = i;
+        double  result = 0;
+        for(int j = 0; j < experimentsNumber; j++)
+        {
+            ModelMananger mananger(i);
+            result += mananger.calcTime();
+        }
+        result /= experimentsNumber;
+
         QLabel* lbl = new QLabel();
-        lbl->setText(QString::number(result));
+        lbl->setText("Memory: " + QString::number(i) + "\tResult: "  + QString::number(result));
         m_ui->centralWidget->layout()->addWidget(lbl);
     }
 
